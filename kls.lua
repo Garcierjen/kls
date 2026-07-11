94c810b7d8ec5c88800f2eb6cc820f4e6df29e0b
Garcierjen <garcierjen@gmail.com>
2026-07-11 17:50:41 +0700

    typewrite

--- kls.lua
+++ kls.lua
@@ -7,6 +7,9 @@ local colord = require("colord")
 local prettytext = require("prettytext")
 
 local function main()
+    prettytext:typewrite(colord:b256setcolor(123, 'fg').."hello world!\n",1)
+    prettytext:wait(1)
+    prettytext:typewrite(colord:b256setcolor(123, 'fg').."kuy\n",1)
     return 0
 end
 
--- prettytext.lua
+++ prettytext.lua
@@ -1,3 +1,26 @@
 local m = {}
 
+---wait in second
+---@param t number
+function m:wait(t)
+    local start = os.clock()
+    while os.clock() - start < t do end
+end
+
+
+--typewrite effect
+---@param text string
+---@param time number
+function m:typewrite(text, time)
+    local len = string.len(text)
+    if len == 0 then return end
+    local del = time / len
+    for i = 1, len do
+        local char = string.sub(text, i, i) 
+        io.write(char)
+        io.flush()
+        m:wait(del)
+    end
+end
+
 return m
